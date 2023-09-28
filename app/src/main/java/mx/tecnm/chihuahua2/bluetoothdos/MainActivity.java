eed14d36e0992d1f54c32d6c748070232443cef8
Jetix24 <josepabloaguirre02@gmail.com>
2023-09-28 17:29:31 -0600

    Bluetooth

---
+++ app/src/main/java/mx/tecnm/chihuahua2/bluetoothdos/MainActivity.java
@@ -0,0 +1,14 @@
+package mx.tecnm.chihuahua2.bluetoothdos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
